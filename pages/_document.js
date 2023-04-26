f9e23cc038bf6853e7eb8a9d69ade0c8f8e1e6c5
Tyrail Leverett <tyrailleverett@gmail.com>
2023-04-26 11:54:47 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-    <meta name='ir-site-verification-token' value='1283089662' />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
         />
       </Head>
       <body>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt USD1283089662 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         <Main />
         <NextScript />
       </body>
